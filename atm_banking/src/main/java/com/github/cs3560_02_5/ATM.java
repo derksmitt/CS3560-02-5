b380fb787306c18529dcae9f045fabe9c078a77f
dancore05 <danuska0cortez@gmail.com>
2026-04-24 21:39:19 -0700

    Update ATM.java
    Improve GUI appearance

--- atm_banking/src/main/java/com/github/cs3560_02_5/ATM.java
+++ atm_banking/src/main/java/com/github/cs3560_02_5/ATM.java
@@ -7,6 +7,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.*;
 import javafx.stage.Stage;
 
@@ -35,49 +36,81 @@ public class ATM extends Application{
         // Set title of window
         window.setTitle("Outlaw Banking");
 
-        // Creating a BorderPane container
-        BorderPane border = new BorderPane();
-        border.setCenter(login());
-
         // Size of scene and display
-        Scene scene = new Scene(border, width, height);
+        Scene scene = new Scene(login(), width, height);
         window.setScene(scene);
         window.show();
     }
 
     /**
-     * Login window displayed to user
+     * Login window displayed to the user
      * @return VBox
      */
-    public VBox login() { 
+    public BorderPane login() { 
+        // Create HBox
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setPadding(new Insets(15,12,15,12));
+        hBox.setStyle("-fx-background-color: #48525d");
+        // Create Text in hBox
+        Text bankNameText = new Text("Outlaw Banking");
+        // Set the style
+        bankNameText.setFont(Font.font("Arial Narrow", FontWeight.THIN, 60));
+        bankNameText.setFill(Color.WHITE);
+        hBox.getChildren().add(bankNameText);
+
         // Create VBox with centering
         VBox layout = new VBox();
         layout.setAlignment(Pos.CENTER);
-        // Create padding and spacing for objects
+        // Set style
+        layout.setStyle("-fx-background-color: #d9d9d9");
+        // Set size
+        layout.setMaxWidth(600);
+        layout.setMaxHeight(300);
+        // Create padding and spacing for objects'
         layout.setPadding(new Insets(90));
         layout.setSpacing(20);
 
-        // Make title name on scene
-        Text bankName = new Text("Outlaw Banking");
-        bankName.setFont(Font.font("Arial", FontWeight.BOLD, 100));
+        // Create new text on top
+        Text name = new Text("Login");
+        // Set style
+        name.setFont(Font.font("Arial Narrow", FontWeight.THIN, 30));
+        name.setFill(Color.rgb(42, 60, 77));
 
+        // Create GridPane
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setVgap(50);
+        grid.setHgap(20);
+
+        // Allow columns to expand
+        grid.getColumnConstraints().addAll(new ColumnConstraints(100), new ColumnConstraints(300));
+
+        // Create text 
+        Text cardNumberText = new Text("Card Number");
         // Defining the card number field
         final TextField cardNum = new TextField();
         cardNum.setPromptText("Enter your Card Number");
-        cardNum.setMaxWidth(500);
+        grid.add(cardNumberText, 0, 0);
+        grid.add(cardNum, 1, 0);
 
+        // Create text
+        Text pinText = new Text("PIN");
         // Defining the pin text field
         final TextField pin = new TextField();
         pin.setPromptText("Enter your PIN");
-        pin.setMaxWidth(500);
+        grid.add(pinText, 0, 1);
+        grid.add(pin, 1, 1);
 
         // Defining the Submit button
-        Button submit = new Button("Login-in");
+        Button submit = new Button("Login");
+        submit.setMaxWidth(100);
+        submit.setStyle("-fx-background-color: #2a3c4d; -fx-text-fill: white");
         // Determine action after button press
         submit.setOnAction(event -> {
                     // Find account linked to cardNumber and pin
                     Account account = authenticateUser(cardNum.getText(), Integer.parseInt(pin.getText()));
-                    // Check if valid account is found
+                    // Check if a valid account is found
                     if (account != null) {
                         // Switch scenes and display alert
                         AlertBox.display("Login Successful", "Welcome " + account.getName());
@@ -88,8 +121,13 @@ public class ATM extends Application{
                     }
                 });
         // Add objects to scene
-        layout.getChildren().addAll(bankName, cardNum, pin, submit);
-        return layout;
+        layout.getChildren().addAll(name, grid, submit);
+
+        // Creating a BorderPane container
+        BorderPane border = new BorderPane();
+        border.setTop(hBox);
+        border.setCenter(layout);
+        return border;
     }
 
     /**
